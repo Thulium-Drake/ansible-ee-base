f4c041caee8637cb28f8b77a8c24eaf8a6893d0c
Jeffrey van Pelt <jeff@vanpelt.one>
2023-10-11 13:47:32 +0200

    Initial working version

--- build_ee.sh
+++ build_ee.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # Build the Execution Environment
-ansible-builder build -t ansible-ee-base:latest
+
+EE_REGISTRY_URL=${1:=git.element-networks.nl/ansible/ee-base}
+
+# Stage 1: Base OS image with all tools installed from Package manager
+ansible-builder build -f execution-environment-stage1.yml -t ansible-ee-base:stage1-latest
+# Stage 2: Extra tools that require tools to be present in image (does not work (yet?) in Stage 1)
+ansible-builder build -f execution-environment-stage2.yml -t ansible-ee-base:stage2-latest
+
+# Upload to Registry as latest
+IMAGE_ID=$(podman image inspect ansible-ee-base:stage2-latest | jq -r .[].Id)
+podman push $IMAGE_ID docker://$EE_REGISTRY_URL:latest
