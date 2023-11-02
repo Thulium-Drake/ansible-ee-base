f826680acbbce6f4420d69b6a1c3ddec0da37f38
Jeffrey van Pelt <jeff@vanpelt.one>
2023-11-02 20:16:08 +0100

    Enable verbose mode for building

--- build_ee.sh
+++ build_ee.sh
@@ -4,9 +4,9 @@
 EE_REGISTRY_URL=${1:=git.element-networks.nl/ansible/ee-base}
 
 # Stage 1: Base OS image with all tools installed from Package manager
-ansible-builder build -f execution-environment-stage1.yml -t ansible-ee-base:stage1-latest
+ansible-builder build -v3 -f execution-environment-stage1.yml -t ansible-ee-base:stage1-latest
 # Stage 2: Extra tools that require tools to be present in image (does not work (yet?) in Stage 1)
-ansible-builder build -f execution-environment-stage2.yml -t ansible-ee-base:stage2-latest
+ansible-builder build -v3 -f execution-environment-stage2.yml -t ansible-ee-base:stage2-latest
 
 # Upload to Registry as latest
 IMAGE_ID=$(podman image inspect ansible-ee-base:stage2-latest | jq -r .[].Id)
