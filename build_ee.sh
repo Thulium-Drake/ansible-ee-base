2f09af98064e8a0e702403950cbde813e5557a87
Jeffrey van Pelt <jeff@vanpelt.one>
2024-01-17 14:32:38 +0100

    Fix package manager path in ubi9, added default variable version for ansible

--- build_ee.sh
+++ build_ee.sh
@@ -5,8 +5,10 @@
 set -e
 
 EE_REGISTRY_URL=${1:=git.element-networks.nl/ansible/ee-base}
+ANSIBLE_VERSION=${2:=2.14}
 
 # Stage 1: Base OS image with all tools installed from Package manager
+sed -i "s/ANSIBLE_VERSION/$ANSIBLE_VERSION/" execution-environment-stage1.yml
 ansible-builder build -v3 -f execution-environment-stage1.yml -t ansible-ee-base:stage1-latest
 # Stage 2: Extra tools that require tools to be present in image (does not work (yet?) in Stage 1)
 ansible-builder build -v3 -f execution-environment-stage2.yml -t ansible-ee-base:stage2-latest
@@ -14,3 +16,6 @@ ansible-builder build -v3 -f execution-environment-stage2.yml -t ansible-ee-base
 # Upload to Registry as latest
 IMAGE_ID=$(podman image inspect ansible-ee-base:stage2-latest | jq -r .[].Id)
 podman push $IMAGE_ID docker://$EE_REGISTRY_URL:latest
+
+# Clean up
+git reset --hard
